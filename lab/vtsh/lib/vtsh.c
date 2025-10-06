1aacea5eb0a81302c4b74b0783788a197a8bd44e
chousik <Zaxap2018@yandex.ru>
2025-10-06 10:15:45 +0300

    lol

--- lab/vtsh/lib/vtsh.c
+++ lab/vtsh/lib/vtsh.c
@@ -5,12 +5,9 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stddef.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
--- lab/vtsh/loaders/ema_replace_str.c
+++ lab/vtsh/loaders/ema_replace_str.c
@@ -5,15 +5,12 @@
 
 #include <errno.h>
 #include <fcntl.h>
-#include <inttypes.h>
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
